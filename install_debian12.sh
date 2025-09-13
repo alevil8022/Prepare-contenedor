02a4ec4227ade685e53871cf473d56e3ae04c9a5
alevil8022 <111132742+alevil8022@users.noreply.github.com>
2025-09-12 23:53:33 -0300

    Create install_debian12.sh

---
+++ install_debian12.sh
@@ -0,0 +1,72 @@
+# ------
+# Instalar Docker en Debian 12
+# ------
+#
+#
+
+function mensaje
+{
+	echo "#--------------------------------------"
+	echo $1
+	echo "#--------------------------------------"
+}
+
+mensaje "Creando usuario Generico"
+useradd -c "av1439" -s /bin/bash av1439
+
+echo "av1439  ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+mensaje "Actualizando ultimos paquetes de SO.   "
+
+apt-get update
+
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt remove $pkg; done
+
+# Add Docker's official GPG key:
+sudo apt update 
+sudo apt install ca-certificates curl 
+sudo install -m 0755 -d /etc/apt/keyrings 
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc 
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \ 
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \ 
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \ 
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null 
+sudo apt update
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+mensaje "Probando docker con Hello World"
+sudo docker run hello-world
+
+mensaje "Subiendo Servicio Docker"
+sudo systemctl status docker
+
+sudo systemctl start docker 
+sudo docker run hello-world
+
+sudo systemctl disable docker 
+sudo systemctl enable docker
+
+
+mensaje "Creando Directorios.     "
+mkdir /contenedores
+cd /contenedores
+mkdir volume
+mkdir docker-compose
+
+
+mensaje "Creando Usuario y grupo   "
+groupadd docker
+usermod -aG docker $USER
+
+mensaje "Agregando Alias en la configuracion"
+echo "alias c='cd /contenedores'" >> ~/.bashrc
+echo "alias v='cd /contenedores/volume'" >> ~/.bashrc
+echo "alias d='cd /contenedores/docker-compose'" >> ~/.bashrc
+
+
+mensaje "Reiniciando"
+systemctl reboot
