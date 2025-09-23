10fac22b0311c823640d9dbaee512676c513977c
alevil8022 <111132742+alevil8022@users.noreply.github.com>
2025-09-23 13:17:47 -0300

    Update install_debian12.sh

--- install_debian12.sh
+++ install_debian12.sh
@@ -30,10 +30,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/doc
 chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
-echo \ 
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \ 
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \ 
-    tee /etc/apt/sources.list.d/docker.list > /dev/null 
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null 
 apt update
 
 apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
