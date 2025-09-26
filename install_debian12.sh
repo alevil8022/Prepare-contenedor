f65a37a73c73209c1e1c392593ca444db3a90f04
alevil8022 <111132742+alevil8022@users.noreply.github.com>
2025-09-25 21:25:35 -0300

    Update install_debian12.sh

--- install_debian12.sh
+++ install_debian12.sh
@@ -11,14 +11,25 @@ function mensaje
 	echo "#--------------------------------------"
 }
 
+mensaje "CAMBIANDO NOMBRE DE LA VM"
+hostnamectl set-hostname <nombredelamaquina>
+
+#-------------------------------------------------------------
+
 mensaje "Creando usuario Generico"
 useradd -c "av1439" -m -d /home/av1439 -s /bin/bash av1439
 
 echo "av1439  ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
+#-------------------------------------------------------------
 mensaje "Actualizando ultimos paquetes de SO.   "
 
 apt update
+apt dist-upgrade
+
+#-------------------------------------------------------------
+
+mensaje "INSTALANDO DOCKER "
 
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt remove $pkg; done
 
@@ -35,45 +46,55 @@ apt update
 
 apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+mensaje "Creando Usuario y grupo   "
+groupadd docker
+usermod -aG docker $USER
+
 mensaje "Probando docker con Hello World"
 docker run hello-world
 
-mensaje "Subiendo Servicio Docker"
-systemctl status docker
-
-systemctl start docker 
-docker run hello-world
-
-systemctl disable docker 
-systemctl enable docker
-
-
+#-------------------------------------------------------------
 mensaje "Creando Directorios.     "
 mkdir /container
 mkdir -p /container/volume
 mkdir -p /container/docker-compose
 
-
-
-mensaje "Creando Usuario y grupo   "
-groupadd docker
-usermod -aG docker $USER
+#-------------------------------------------------------------
 
 mensaje "Agregando Alias en la configuracion"
 echo "alias c='cd /container'" >> ~/.bashrc
 echo "alias v='cd /container/volume'" >> ~/.bashrc
 echo "alias d='cd /container/docker-compose'" >> ~/.bashrc
 
+#-------------------------------------------------------------
+
 mensaje "ACTUALIZANDO EL BANNER DE LA VM"
 echo '#!/bin/bash'; while IFS= read -r line; do echo "echo '$line'"; done < banner/banner.txt > mymotd.sh
 cp mymotd.sh /etc/profile.d/
 chmod +x /etc/profile.d/mymotd.sh
 systemctl restart sshd
+#-------------------------------------------------------------
+
+mensaje "INSTALANDO HTOP"
+
+apt install htop
+
+#-------------------------------------------------------------
+
+mensaje "INSTALANDO TAILSCALE"
+
+curl -fsSL https://tailscale.com/install.sh | sh
+
+echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
+
+sudo tailscale up
+
+sudo tailscale set --advertise-exit-node
 
 
-mensaje "CAMBIANDO NOMBRE DE LA VM"
-hostnamectl set-hostname io-cocuy-vpn
-
+#-------------------------------------------------------------
 
 mensaje "Reiniciando"
 systemctl reboot
