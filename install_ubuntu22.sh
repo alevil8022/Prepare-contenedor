57d781ce14a52b40e178c786c7b3ceb8e69fad40
alevil8022 <111132742+alevil8022@users.noreply.github.com>
2025-09-25 21:33:19 -0300

    Update install_ubuntu22.sh

--- install_ubuntu22.sh
+++ install_ubuntu22.sh
@@ -12,18 +12,33 @@ function mensaje
 }
 
 mensaje "CAMBIANDO NOMBRE DE LA VM"
-hostnamectl set-hostname nuevonombre
+hostnamectl set-hostname <nombredelamaquina>
+
+#-------------------------------------------------------------
 
 mensaje "Creando usuario Generico"
-useradd -c "usuario" -s /bin/bash av1439
+useradd -c "av1439" -m -d /home/av1439 -s /bin/bash av1439
 
-mensaje "CONFIGURANDO VISUDO"
-echo "usuario  ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+echo "av1439  ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
+#-------------------------------------------------------------
 
+mensaje "CAMBIANDO PASSWORD USUARIO"
+# Generar el hash de la contraseña "MiPassword123"
+HASH_PASSWORD=$(openssl passwd -6 'MiPassword123')
+
+# Establecer la contraseña encriptada para el usuario 'miusuario'
+echo "av1439:${HASH_PASSWORD}" | sudo chpasswd -e
+
+#-------------------------------------------------------------
 mensaje "Actualizando ultimos paquetes de SO.   "
 
-apt-get update
+apt update
+apt dist-upgrade
+
+#-------------------------------------------------------------
+
+mensaje "INSTALANDO DOCKER "
 
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
@@ -48,27 +63,56 @@ apt-get update
 apt-get install docker-compose-plugin
 
 
-mensaje "Creando Directorios.     "
-mkdir -p /container/volume
-mkdir -p /container/docker-compose
 
 mensaje "Creando Usuario y grupo   "
 groupadd docker
 usermod -aG docker $USER
+
+mensaje "Probando docker con Hello World"
+docker run hello-world
+
+#-------------------------------------------------------------
+mensaje "Creando Directorios.     "
+mkdir /container
+mkdir -p /container/volume
+mkdir -p /container/docker-compose
+
+#-------------------------------------------------------------
 
 mensaje "Agregando Alias en la configuracion"
 echo "alias c='cd /container'" >> ~/.bashrc
 echo "alias v='cd /container/volume'" >> ~/.bashrc
 echo "alias d='cd /container/docker-compose'" >> ~/.bashrc
 
+#-------------------------------------------------------------
 
 mensaje "ACTUALIZANDO EL BANNER DE LA VM"
 echo '#!/bin/bash'; while IFS= read -r line; do echo "echo '$line'"; done < banner/banner.txt > mymotd.sh
 cp mymotd.sh /etc/profile.d/
 chmod +x /etc/profile.d/mymotd.sh
 systemctl restart sshd
+#-------------------------------------------------------------
+
+mensaje "INSTALANDO HTOP"
+
+apt install htop
+
+#-------------------------------------------------------------
+
+mensaje "INSTALANDO TAILSCALE"
+
+curl -fsSL https://tailscale.com/install.sh | sh
+
+echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
+
+sudo tailscale up
+
+sudo tailscale set --advertise-exit-node
 
 
+#-------------------------------------------------------------
 
 mensaje "Reiniciando"
 systemctl reboot
